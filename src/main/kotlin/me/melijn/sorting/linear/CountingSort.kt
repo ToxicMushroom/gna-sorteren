b2cacde3ea274745a63d39dc977c06e7276be8f0
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-08-10 23:57:35 +0200

    refactor: add a function for clarification add some docs, fix some docs

--- src/main/kotlin/me/melijn/sorting/linear/CountingSort.kt
+++ src/main/kotlin/me/melijn/sorting/linear/CountingSort.kt
@@ -12,15 +12,18 @@ object CountingSort {
 
         val N = toSort.size
         val R = 26
-        val count = Array(R + 1) { 0 }
+        val count = Array(R + 1) { 0 } // [0, 0, 0, 0]
         val aux = Array(toSort.size) { '0' }
 
-        for (i in 0 until N) count[toSort[i].lowercaseChar().code - 'a'.code + 1]++
-        for (r in 0 until R) count[r + 1] += count[r]
-        for (i in 0 until N) aux[count[toSort[i].lowercaseChar().code - 'a'.code]++] = toSort[i]
-        for (i in 0 until N) toSort[i] = aux[i]
+        for (i in 0 until N) count[getValuesCountingIndex(toSort, i) + 1]++ // Populate the array with character counts [0, 4, 5, 3]
+        for (r in 0 until R) count[r + 1] += count[r] // Make the counts start counting on top of another (cumulative) [0, 4, 9, 12]
+        for (i in 0 until N) aux[count[getValuesCountingIndex(toSort, i)]++] = toSort[i] // try to understand this by maybe writing it on paper :)
+        for (i in 0 until N) toSort[i] = aux[i] // propagate aux array back into toSort
         return toSort
     }
+
+    private fun getValuesCountingIndex(toSort: MutableList<Char>, i: Int) =
+        toSort[i].lowercaseChar().code - 'a'.code
 
 }
 
@@ -32,5 +35,5 @@ fun main() {
 // Uses 8N + 3R + 1 array accesses to sort N records whose keys are between 0 and R-1
 // Uses extra space proportional to N + R
 // Only useful if you have many duplicate keys!
-// + Sorting people by age
-// - Sorting people by birthday
+//   good for sorting people by age
+//   not good for sorting people by birthday
